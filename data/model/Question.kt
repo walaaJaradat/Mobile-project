d39b1d8e54249405f16e1080f4c33bd2862ef93d
TuleenRimawi <rimawituleen@gmail.com>
2026-06-14 21:12:10 +0300

    Delete data directory

--- data/model/Question.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mvvm.data.model
-
-data class Question(
-    val id: Int,
-    val question: String,
-    val options: List<String>,
-    val correctAnswer: String,
-    val category: String
-)
